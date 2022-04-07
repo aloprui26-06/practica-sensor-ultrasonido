9c72d5dc1ca895e7bcec306f9835fef4a66ddd73
aloprui26-06 <102860876+aloprui26-06@users.noreply.github.com>
2022-04-07 15:39:19 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,7 +9,14 @@ basic.showLeds(`
 soundExpression.giggle.play()
 basic.forever(function () {
     distancia = maqueen.Ultrasonic(PingUnit.Centimeters)
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 70)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 35)
+    basic.pause(1200)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 35)
+    basic.pause(1500)
+    maqueen.motorStop(maqueen.Motors.M2)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 35)
+    basic.pause(1200)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 35)
     if (distancia < 10) {
         maqueen.motorStop(maqueen.Motors.All)
         soundExpression.spring.play()
@@ -17,7 +24,7 @@ basic.forever(function () {
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 35)
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 35)
         basic.pause(1000)
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 70)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 35)
         basic.showLeds(`
             . . . . .
             . # . # .
